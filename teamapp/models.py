536dbece93d513c2d8fe396a7dd8a33cc6cb1e23
s1f102101615 <s1f102101615@iniad.org>
2021-12-10 17:12:49 +0900

    def post実装

--- teamapp/models.py
+++ teamapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from teamapp.models import Article
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 # Create your models here.
+
+def post(request):
+    return  render(request, 'teamapp/post.html')
