f28e337c0adee3e1f5716171c427b495b1cfa4e8
naoki07072001 <s1f102101457@iniad.org>
2021-12-17 17:58:28 +0900

    urls

--- teamapp/urls.py
+++ teamapp/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('post',views.post,name='post'),
 
-    path('<int:article_id>/update', views.update, name='update'),
-    path('hello', views.hello, name='hello'),
-    path('redirect', views.redirect_test, name='redirect_test'),
-    path('<int:article_id>/', views.detail, name='detail'),
-    path('<int:article_id>/delete', views.delete, name='delete'),
-    path('<int:article_id>/like',views.like, name='like'),
-    path('api/articles/<int:article_id>/like',views.api_like),
+    #path('<int:article_id>/update', views.update, name='update'),
+    #path('hello', views.hello, name='hello'),
+    #path('redirect', views.redirect_test, name='redirect_test'),
+    #path('<int:article_id>/', views.detail, name='detail'),
+    #path('<int:article_id>/delete', views.delete, name='delete'),
+    #path('<int:article_id>/like',views.like, name='like'),
+    #path('api/articles/<int:article_id>/like',views.api_like),
 ]
